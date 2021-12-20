0823ab48b83f3a57040b01691b6b9e2bdbecba85
vijaysharma723 <vijaysharma723@gmail.com>
2021-12-21 01:26:56 +0530

    resolved marks issues

--- all-actors/src/main/java/org/sunbird/CertRegistryAsyncService.java
+++ all-actors/src/main/java/org/sunbird/CertRegistryAsyncService.java
@@ -14,29 +14,14 @@ import play.libs.Json;
 public class CertRegistryAsyncService {
     private static Logger logger = LoggerFactory.getLogger(CertRegistryAsyncService.class);
 
-    public static String makeAsyncPostCall(String apiToCall, String requestBody, Map<String, String> headerMap) throws IOException {
+    public static void makeAsyncPostCall(String apiToCall, String requestBody, Map<String, String> headerMap) throws IOException {
         logger.info(":makePostCall:get request to make post call for API:" + apiToCall);
-        String result = "";
         Future<HttpResponse<JsonNode>> jsonResponse
                 = Unirest.post(apiToCall)
                 .headers(headerMap)
                 .body(requestBody)
                 .asJsonAsync();
-        try {
-            //logger.info("response from cert-registry: " + jsonResponse.get().getBody());
-            //String result =
-            //com.fasterxml.jackson.databind.JsonNode jsonnd = jsonResponse.get().getBody();
-
-            com.mashape.unirest.http.JsonNode jsonnd = jsonResponse.get().getBody();
-            result  = (String) jsonnd.getObject().get("interpretation");
-            logger.info("result"+ result);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return result;
+        logger.info("response from cert-registry: " + jsonResponse);
 //        Unirest.shutdown() ;
     }
 }
